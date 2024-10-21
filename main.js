cff43895fcc44bf2a95470c9bd7b8799ef5de8b3
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2024-10-21 04:30:34 +0100

    I added the function to increment the scores of both the user and the computer

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-
-
-const Rock = document.querySelector('.btn1')
-const Scissors = document.querySelector('.btn2')
-const Paper = document.querySelector('.btn3')
 let humanScore = 0;
 let computerScore = 0;
 
@@ -17,7 +12,6 @@ function getComputerChoice() {
         return 'paper';
     }
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
 
@@ -33,34 +27,6 @@ function getHumanChoice() {
         return 'Input wrong';
     }
 }
-// console.log(getHumanChoice())
-
-
-// function disableButtons() {
-//     Rock.disabled = true;
-//     Scissors.disabled = true;
-//     Paper.disabled = true;
-// }
-
-// function rock() {
-//     console.log('Rock');
-//     disableButtons()
-// }
-
-// function scissors() {
-//     console.log('scissors');
-//     disableButtons()
-// }
-
-// function paper() {
-//     console.log('paper');
-//     disableButtons()
-// }
-
-// Rock.addEventListener('click', rock);
-// Scissors.addEventListener('click', scissors);
-// Paper.addEventListener('click', paper);
-
 
 
 
@@ -70,16 +36,24 @@ function playRound(human, computer) {
 // const computerChoice = getComputerChoice();
 // console.log(humanChoice);
 
+let result;
+
     if (human === computer){
-        return 'the game is tied';
+        result = 'the game is tied';
     } else if ((human === 'rock' && computer === 'scissors') || (human === 'scissors' && computer === 'paper') || (human === 'paper' && computer === 'rock')){
-        return 'You won';
+        result = `You won! ${human} beats ${computer}`;
+        humanScore++;
     } else {
-        return 'You lose, computer wins';
+        result = `You lose, computer wins! ${computer} beats ${human}`;
+        computerScore++;
     }
+
+    console.log(result);
+    console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
 }
 const humanChoice = getHumanChoice();
 const computerChoice = getComputerChoice();
 console.log(humanChoice);
+console.log(computerChoice);
 
-console.log(playRound(humanChoice, computerChoice));
+playRound(humanChoice, computerChoice);
